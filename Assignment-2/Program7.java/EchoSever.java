279c0ce98f8284f4f152213bca8fbe2c3c787d5d
jitikshapatel <pateljitiksha31@gmail.com>
2026-03-21 23:20:29 +0500

    EchoSever.java

---
+++ Assignment-2/Program7.java/EchoSever.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+
+class EchoServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(5000);
+            System.out.println("Server started... Waiting for client");
+
+            Socket socket = server.accept();
+            System.out.println("Client connected");
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            String msg;
+
+            while ((msg = in.readLine()) != null) {
+                System.out.println("Client: " + msg);
+                out.println("Echo: " + msg);
+
+                if (msg.equalsIgnoreCase("exit"))
+                    break;
+            }
+
+            socket.close();
+            server.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
